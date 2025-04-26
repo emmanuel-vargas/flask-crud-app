ef1245307369c4a22244d10a2065c06ad5af6f79
Emmanuel <63261177+emmanuel-vargas@users.noreply.github.com>
2025-04-26 07:12:45 -0600

    Initial commit - Flask CRUD

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+usuarios = []
+
+@app.route('/')
+def index():
+    return render_template('index.html', usuarios=usuarios)
+
+@app.route('/crear', methods=['POST'])
+def crear():
+    nombre = request.form['nombre']
+    email = request.form['email']
+    usuarios.append({'nombre': nombre, 'email': email})
+    return redirect(url_for('index'))
+
+@app.route('/editar/<int:id>', methods=['GET', 'POST'])
+def editar(id):
+    if request.method == 'POST':
+        usuarios[id]['nombre'] = request.form['nombre']
+        usuarios[id]['email'] = request.form['email']
+        return redirect(url_for('index'))
+    return render_template('editar.html', usuario=usuarios[id], id=id)
+
+@app.route('/eliminar/<int:id>')
+def eliminar(id):
+    usuarios.pop(id)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
